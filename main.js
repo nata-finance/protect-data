d7871b11df048a0b14d735a487dfd784d80e2e4e
xalava <xavier@bricodeur.com>
2023-11-04 22:00:17 +0000

    added the contact section

--- main.js
+++ main.js
@@ -1,59 +1,47 @@
-// import { ethers } from 'ethers';
-
 import { IExecDataProtector } from "@iexec/dataprotector"
 
-
-const init = async () => {
- if (!window.ethereum) {
-    throw Error('missing injected ethereum provider in page');
-  }
-
-  await window.ethereum.request({
-    method: 'eth_requestAccounts',
-  });
+const { VITE_PUBLIC_KEY, VITE_WEB3MAIL_ADDRESS, VITE_PRIVATE_KEY, VITE_PROTECTED_DATA} = import.meta.env;
+const initWeb3Provider = async () => {
+	if (!window.ethereum) {
+		throw Error('missing injected ethereum provider in page')
+	}
+	await window.ethereum.request({
+		method: 'eth_requestAccounts',
+	})
+	return window.ethereum
 }
-init()
+const web3Provider = await initWeb3Provider()
 
-const web3Provider = window.ethereum
-// instantiate
 const dataProtector = new IExecDataProtector(web3Provider)
 
 const register = async (tvl, email) => {
-	// const result = await 
-	const computedTVL = Math.pow(10,Math.floor(Math.log10(tvl)))
+	const computedTVL = Math.floor(Math.log10(tvl))
 	dataProtector.protectData({
 		data: {
 			email: email,
 			TVL: computedTVL
-		}
+		},
+		name: computedTVL + "nata" // hack to make it indexable. 
 	}).then(result => {
-		document.getElementById('result').innerHTML = result
-		console.log(result)
-})
-		.catch(err => console.log(err))
+		document.getElementById('result').innerHTML = "Data protected 🔐 " + result.address
+		const grantAccessBtn = document.createElement('button')
+		grantAccessBtn.innerHTML = 'Grant Access'
+		grantAccessBtn.addEventListener('click', () => {
+			grantAccess(result.address)
+		})
+		document.getElementById('result').appendChild(grantAccessBtn)
+	})
+	.catch(err => console.log(err))
 }
 
-// const registerOBS = async (tvl, email) => {
-// 	dataProtector
-// 		.protectDataObservable({
-// 			data: {
-// 				email: email,
-// 				TVL: computedTVL,
-// 			},
-// 			name: 'DaoContact',
-// 		})
-// 		.subscribe(
-// 			(data) => {
-// 				console.log("➡ data:", data)
-// 				if (data.message === 'PROTECTED_DATA_DEPLOYMENT_SUCCESS') {
-// 					console.log("✅", data.address)
-// 				}
-// 			}
-// 			,
-// 			(e) => console.log("➡ e:", e),
-// 			() => console.log('➡ DONE')
-// 		)
-// };
+const grantAccess = async (address) => {
+	const grantedAccess = await dataProtector.grantAccess({
+		protectedData: address,
+		authorizedApp: VITE_WEB3MAIL_ADDRESS,
+		authorizedUser: VITE_PUBLIC_KEY
+	})
+	console.log(grantedAccess)
+}
 
 const registerBtn = document.getElementById('register-btn')
 const emailInput = document.getElementById('email-input')
@@ -64,3 +52,62 @@ registerBtn.addEventListener('click', () => {
 	const tvl = tvlInput.value
 	register(tvl,email)
 })
+
+const listProtectedData = await dataProtector.fetchProtectedData({
+	schema: {
+		"email": "string",
+		"TVL": "number"
+	}
+})
+
+const MAX_CATEGORY = 15
+let contactsPerCategory = Array(MAX_CATEGORY).fill(0)
+let emailAddresses = []
+console.log(contactsPerCategory)
+listProtectedData.map(data => {
+	if (data.name.endsWith("nata")) {
+		let category = data.name.slice(0, -4)
+		console.log("💡", category, data)
+		contactsPerCategory[category]+=1
+		emailAddresses.push(data.address)
+	}
+})
+
+console.log(contactsPerCategory)
+
+for (let i = 0; i < MAX_CATEGORY; i++) {
+	if (contactsPerCategory[i] > 0) {
+		const dataDiv = document.createElement('li')
+		dataDiv.innerHTML = ` ${Math.pow(10, i)} and ${Math.pow(10, i+1)}`
+		document.getElementById('list-protected-data').appendChild(dataDiv)
+		const dataSpan = document.createElement('span')
+		dataSpan.innerHTML = `(${contactsPerCategory[i]} sellers)`
+		dataSpan.className = 'right grey'
+		dataDiv.appendChild(dataSpan)
+		const dataBtn = document.createElement('button')
+		dataBtn.innerHTML = `Contact`
+		dataBtn.className = 'right'
+		dataDiv.appendChild(dataBtn)
+		dataBtn.addEventListener('click', () => {
+			console.log("Contact" + i)
+			console.log(contactsPerCategory[i])
+			const messageInput = document.getElementById('message-input')
+			sendMail(messageInput.value, VITE_PROTECTED_DATA)
+		})
+	}
+}
+
+import { IExecWeb3mail } from "@iexec/web3mail"
+
+const web3mail = new IExecWeb3mail(web3Provider) 
+
+const sendMail = async (emailContent, protectedData) => {
+	const emailSubject = 'You have received an message from a potential acquirer - Nata'
+	const result = await web3mail.sendEmail({
+		protectedData,
+		emailSubject,
+		emailContent
+	})
+
+	console.log(result)
+}
